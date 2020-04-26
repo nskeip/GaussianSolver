3953bd6d0fde1b9ba9a232f174fc2a8881fe034b
Nikita Hismatov <hismatovns@gmail.com>
2020-04-26 12:10:26 +0500

    list-based approach (less memory efficient)

--- solver.py
+++ solver.py
@@ -1,4 +1,3 @@
-from collections import defaultdict
 from typing import List, Tuple, TypeVar
 
 
@@ -9,17 +8,33 @@ class Matrix:
     def __init__(self, rows: int, cols: int):
         self.rows = rows
         self.cols = cols
-        self._values_store = defaultdict(lambda: 0)
+        self.values = [0] * rows * cols
 
-    def to_list_of_rows(self) -> List[List[Numeric]]:
-        return [
-            [self._values_store[r, c] for c in range(self.cols)]
-            for r in range(self.rows)
+    def __getitem__(self, item: Tuple[int, int]):
+        return self.values[self.coordinate_to_index(item)]
+
+    def __setitem__(self, key: Tuple[int, int], new_value: Numeric):
+        self.values = [
+            v if i != self.coordinate_to_index(key) else new_value
+            for i, v in enumerate(self.values)
         ]
-
-    def __setitem__(self, key: Tuple[int, int], value: Numeric):
-        self._values_store[key] = value
 
     def __str__(self):
         cols_fmt = "\t".join(["{:10f}"] * self.cols)
-        return "\n".join(cols_fmt.format(*r).strip() for r in (self.to_list_of_rows()))
+        return "\n".join(
+            cols_fmt.format(*self.row_as_list(row_n)).strip()
+            for row_n in range(self.rows)
+        )
+
+    def coordinate_to_index(self, coord_tuple: Tuple[int, int]) -> int:
+        row, col = coord_tuple
+        return row * self.cols + col
+
+    def row_as_list(self, n: int) -> List[int]:
+        start = self.coordinate_to_index((n, 0))
+        return self.values[start : start + self.cols]
+
+    def col_as_list(self, n: int) -> List[int]:
+        start = self.coordinate_to_index((0, n))
+        stop = self.coordinate_to_index((self.rows - 1, n + 1))
+        return self.values[start : stop : self.cols]
--- test_solver.py
+++ test_solver.py
@@ -1,3 +1,5 @@
+import itertools
+
 from solver import *
 
 
@@ -5,11 +7,24 @@ class TestMatrix:
     m: Matrix
 
     def setup(self):
-        self.m = Matrix(3, 3)
+        self.m = Matrix(4, 4)
 
-    def test_empty_list_to_list(self):
-        assert self.m.to_list_of_rows() == [[0] * 3] * 3
+    def test_get_set_item(self):
+        self.m[1, 1] = 1
+        assert self.m[1, 1] == 1
+        for a, b in itertools.product(range(self.m.rows), range(self.m.cols)):
+            if a != 1 and b != 1:
+                assert self.m[a, b] == 0
 
-    def test_set_item(self):
-        self.m[0, 0] = 1
-        assert self.m.to_list_of_rows() == [[1, 0, 0]] + [[0] * 3] * 2
+    def test_row_col_as_list(self):
+        self.m[1, 1] = 1
+        assert self.m.row_as_list(1) == [0, 1, 0, 0]
+        assert self.m.col_as_list(1) == [0, 1, 0, 0]
+
+    @staticmethod
+    def test_coordinate_to_index():
+        m = Matrix(rows=4, cols=5)
+        assert m.coordinate_to_index((0, 0)) == 0
+        assert m.coordinate_to_index((3, 4)) == 19
+        assert m.coordinate_to_index((1, 0)) == 5
+        assert m.coordinate_to_index((1, 1)) == 6
