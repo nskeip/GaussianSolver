1df250bef9d8d8ca4cd9303961e0e08aa186ac78
Nikita Hismatov <hismatovns@gmail.com>
2020-04-26 10:07:22 +0500

    removed test_str - as it is okay already :)

--- test_solver.py
+++ test_solver.py
@@ -13,12 +13,3 @@ class TestMatrix:
     def test_set_item(self):
         self.m[0, 0] = 1
         assert self.m.to_list_of_lists() == [[1, 0, 0]] + [[0] * 3] * 2
-
-    def test_str(self):
-        self.m[0, 0] = 5000
-        print(self.m)
-        assert (
-            str(self.m) == "5000.000000       0.000000        0.000000\n"
-            "0.000000          0.000000        0.000000\n"
-            "0.000000          0.000000        0.000000"
-        )
