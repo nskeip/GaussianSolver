7df9aca2f3a1194852c8e588e93075526c5c6298
Nikita Hismatov <hismatovns@gmail.com>
2020-04-26 09:28:24 +0500

    TestMatrix

---
+++ test_solver.py
@@ -0,0 +1,15 @@
+from solver import *
+
+
+class TestMatrix:
+    m: Matrix
+
+    def setup(self):
+        self.m = Matrix(3, 3)
+
+    def test_empty_list_to_list(self):
+        assert self.m.to_list_of_lists() == [[0] * 3] * 3
+
+    def test_set_item(self):
+        self.m[0, 0] = 1
+        assert self.m.to_list_of_lists() == [[1, 0, 0]] + [[0] * 3] * 2
